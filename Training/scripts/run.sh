c640269850a9ac286c5075c3838c6dc9a75441be
yszhang95 <zysright@mail.ustc.edu.cn>
2021-02-08 23:11:55 -0600

    fix issue in configs

--- Training/scripts/run.sh
+++ Training/scripts/run.sh
@@ -4,7 +4,7 @@ echo "Running on: `uname -a`" #Condor job is running on this node
 echo "System software: `cat /etc/redhat-release`" #Operating System on that node
 # CMSSW
 source /cvmfs/cms.cern.ch/cmsset_default.sh 
-export SCRAM_ARCH=slc7_amd64_gcc630
+export SCRAM_ARCH=slc7_amd64_gcc530
 scramv1 project CMSSW CMSSW_8_0_35
 cd CMSSW_8_0_35/src/
 eval `scramv1 runtime -sh`
@@ -16,11 +16,11 @@ pwd
 echo "Arguments passed to the job: $1"
 
 git clone https://github.com/yszhang95/OpenHF2020.git
-cd Training
+cd OpenHF2020/Training
 mkdir bin
 make
 cd test
-../bin/tmvaClassfication "../Configs/$1"
+../bin/tmvaClassfication ../Configs/$1
 cd ../
 
 cd ${_CONDOR_SCRATCH_DIR}
