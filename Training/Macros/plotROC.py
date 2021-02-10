c91406d1b0b572a2f45a1b9fece718ddc3bfd875
Yousen Zhang <zysright@gmail.com>
2021-02-10 08:20:58 +0100

    update configs for variable list comparison

--- Training/Macros/plotROC.py
+++ Training/Macros/plotROC.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python3
 import ROOT as r
+r.gROOT.SetBatch()
 
 infiles = [
-'../test/TMVA_topo4_dau3trans_BDTG.root',
-'../test/TMVA_topo4_dau4_BDTG.root',
-'../test/TMVA_topo8_BDTG.root',
-'../test/TMVA_topo8_dau3trans_BDTG.root',
-'../test/TMVA_topo4_CutsOnKs_BDTG.root'
+'../test/VarsBDTA/TMVA_topo4_CutsOnKs_BDTA_100_Depth3_Beta0.8.root',
+'../test/VarsBDTA/TMVA_topo4_dau4_BDTA_100_Depth3_Beta0.8.root',
+'../test/VarsBDTA/TMVA_topo4_dauEtaPtTrans_BDTA_100_Depth3_Beta0.8.root',
+'../test/VarsBDTA/TMVA_topo4_dauEta_dauPtTrans_BDTA_100_Depth3_Beta0.8.root',
+'../test/VarsBDTA/TMVA_topo8_BDTA_100_Depth3_Beta0.8.root',
+'../test/VarsBDTA/TMVA_topo8_dau4_BDTA_100_Depth3_Beta0.8.root',
+'../test/VarsBDTA/TMVA_topo8_dauEtaPtTrans_BDTA_100_Depth3_Beta0.8.root',
         ]
-colors = [1, 2, 3, 4, 5, 6]
+colors = [1, 2, 3, 4, 5, 6, 7]
 
 fs = [ r.TFile.Open(f) for f in infiles ]
-hists = [f.Get('Method_BDT/BDTG/MVA_BDTG_trainingRejBvsS')\
+hists = [f.Get('Method_BDT/BDTA/MVA_BDTA_trainingRejBvsS')\
             .Clone(f.GetName()\
-            .replace("../test/TMVA_", "").replace(".root", "")) \
+            .replace("../test/VarsBDTA/TMVA_", "").replace(".root", "")) \
             for f in fs]
 c = r.TCanvas("cROC", "", 800, 600)
 hFrame = c.DrawFrame(0, 0, 1, 1);
 hFrame.SetTitle("Background rejection versus Signal efficiency;Background rejection;Signal efficiency")
-leg = r.TLegend(0.15, 0.15, 0.5, 0.4)
+leg = r.TLegend(0.15, 0.15, 0.55, 0.45)
 for (h, col, f) in zip(hists, colors, infiles):
     h.SetLineColor(col)
     h.Draw("SAME")
-    leg.AddEntry(h, f.replace("../test/TMVA_", "").replace(".root", ""), "l")
+    leg.AddEntry(h, f.replace("../test/VarsBDTA/TMVA_", "").replace(".root", ""), "l")
 leg.SetBorderSize(0)
 leg.Draw()
 
