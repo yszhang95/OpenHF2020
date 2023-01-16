f5758aa2decfc0acfb6302e019c73338f3ac934c
Yousen Zhang <zysright@gmail.com>
2023-01-16 16:08:13 +0100

    added scripts for picking bad lists and rerunning

---
+++ ProduceNTuples/SkimNoMVA/20230114/dropEmptyFiles.C
@@ -0,0 +1,43 @@
+std::vector<std::string> getFileNames(const std::string& files)
+{
+    std::ifstream infile(files.c_str());
+
+    std::vector<std::string> names;
+    std::string temp;
+    while (std::getline(infile, temp)) {
+        if (temp.empty()) continue;
+        TString n(temp.c_str());
+        n = n.Strip(TString::kBoth);
+        if (n[0] == '#') continue;
+        names.push_back(temp);
+    }
+    return names;
+}
+void dropEmptyFiles(const std::string& files)
+{
+    std::string base = gSystem->BaseName(files.c_str());
+    std::ofstream fgood("good/" + base);
+    std::ofstream fbad("bad/" + base);
+    auto names = getFileNames(files);
+
+    std::cout << "To process " << names.size() << " files\n";
+
+    for (const auto& n : names) {
+        TString nn(n.c_str());
+        nn = nn.Strip(TString::kBoth);
+        std::cout << "Checking " << nn << "\n";
+        TFile* f = TFile::Open(nn.Data());
+        if (!f->IsOpen()) { std::cout << "Bad file\n"; continue; }
+        auto t = f->Get<TTree>("lambdacAna/ParticleTree");
+        if (t) {
+            // std::cout << "Good tree\n";
+            fgood << nn << "\n";
+
+        } else {
+            // std::cout << "Non-existing tree\n";
+            fbad << nn << "\n";
+        }
+        delete f;
+        // std::cout << "Finished\n";
+    }
+}
---
+++ ProduceNTuples/SkimNoMVA/20230114/rerun_Pbp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ROOT_INCLUDE_PATH=${ROOT_INCLUDE_PATH}:${OPENHF2020TOP} \
+    ${OPENHF2020TOP}/Training/Macros/skimTree.py --pTMin 1.9 \
+    --wantAbort --selectDeDx --flipEta \
+    -i $1
