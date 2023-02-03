7f343edf2df200163485a9db5e9dc1065223e9c9
Yousen Zhang <zysright@gmail.com>
2023-02-03 16:36:17 +0100

    D0 production config

---
+++ D0Productions/cmssw_latest/pPbSkimAndTree2016_D0Both_BDT_ForLamC_cfg.py
@@ -0,0 +1,160 @@
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("ANASKIM")
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+process.MessageLogger.cerr.FwkReport.reportEvery = 200
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+#'/store/hidata/PARun2016C/PAHighMultiplicity1/AOD/PromptReco-v1/000/285/505/00000/006F1E14-85AF-E611-9F9E-02163E014508.root'
+'file:output.root'
+)
+)
+
+# =============== Other Statements =====================
+# process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(2000))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10))
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+process.GlobalTag.globaltag = '80X_dataRun2_v19'
+
+# =============== Import Sequences =====================
+#Trigger Selection
+### Comment out for the timing being assuming running on secondary dataset with trigger bit selected already
+# Add trigger selection
+import HLTrigger.HLTfilters.hltHighLevel_cfi
+process.hltFilter = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+process.hltFilter.andOr = cms.bool(True)
+process.hltFilter.throw = cms.bool(False)
+process.hltFilter.HLTPaths = [
+    'HLT_PAFullTracks_Multiplicity120_v*', # High multiplicity
+    'HLT_PAFullTracks_Multiplicity150_v*', # High multiplicity
+    'HLT_PAFullTracks_Multiplicity185_part*', # High multiplicity
+    'HLT_PAFullTracks_Multiplicity250_v*', # High multiplicity
+    'HLT_PAL1MinimumBiasHF_OR_SinglePixelTrack_part*', # Minimum bias
+    ]
+
+# Add PbPb collision event selection
+process.load('VertexCompositeAnalysis.VertexCompositeProducer.collisionEventSelection_cff')
+#process.colEvtSel = cms.Sequence(process.hfCoincFilter * process.primaryVertexFilterPA * process.NoScraping * process.olvFilter_pPb8TeV_dz1p0)
+#remove the default dz1p0 filter
+process.colEvtSel = cms.Sequence(process.hfCoincFilter * process.primaryVertexFilterPA * process.NoScraping)
+
+# Define the event selection sequence
+process.eventFilter_HM = cms.Sequence(
+    process.hltFilter *
+    process.colEvtSel
+)
+process.eventFilter_HM_step = cms.Path( process.eventFilter_HM )
+
+########## D0 candidate rereco ###############################################################
+process.load("VertexCompositeAnalysis.VertexCompositeProducer.generalD0Candidates_cff")
+process.generalD0CandidatesNew = process.generalD0Candidates.clone()
+process.generalD0CandidatesNew.trkPtSumCut = cms.double(1.6)
+process.generalD0CandidatesNew.trkEtaDiffCut = cms.double(1.0)
+process.generalD0CandidatesNew.tkNhitsCut = cms.int32(11)
+process.generalD0CandidatesNew.tkPtErrCut = cms.double(0.1)
+process.generalD0CandidatesNew.tkPtCut = cms.double(0.7)
+process.generalD0CandidatesNew.alphaCut = cms.double(1.0)
+process.generalD0CandidatesNew.alpha2DCut = cms.double(1.0)
+process.generalD0CandidatesNew.dPtCut = cms.double(1.9)
+
+process.generalD0CandidatesNewWrongSign = process.generalD0CandidatesNew.clone(isWrongSign = cms.bool(True))
+
+process.d0rereco_step = cms.Path( process.eventFilter_HM * process.generalD0CandidatesNew )
+process.d0rereco_wrongsign_step = cms.Path( process.eventFilter_HM * process.generalD0CandidatesNewWrongSign )
+
+
+# produce D0 trees
+process.load("VertexCompositeAnalysis.VertexCompositeAnalyzer.d0selector_cff")
+process.load("VertexCompositeAnalysis.VertexCompositeAnalyzer.d0analyzer_tree_cff")
+process.load("VertexCompositeAnalysis.VertexCompositeAnalyzer.eventinfotree_cff")
+
+process.TFileService = cms.Service("TFileService",
+    fileName =
+    cms.string('d0ana_tree.root')
+    )
+
+# set up selectors
+process.d0selector = process.d0selectorBDTPreCut.clone()
+process.d0selector.GBRForestFileName = cms.string('GBRForestfile_BDT_PromptD0InpPb_default_HLT185_WS_Pt1p5MassPeak_v2.root')
+process.d0selector.multMin = cms.untracked.double(0)
+process.d0selector.multMax = cms.untracked.double(100000)
+process.d0selectorWS = process.d0selector.clone(
+    VertexCompositeCollection = cms.untracked.InputTag("generalD0CandidatesNewWrongSign:D0"),
+    MVACollection = cms.InputTag("generalD0CandidatesNewWrongSign:MVAValues")
+)
+
+process.d0ana.useAnyMVA = cms.bool(True)
+process.d0ana.multMin = cms.untracked.double(0)
+process.d0ana.multMax = cms.untracked.double(100000)
+process.d0ana.VertexCompositeCollection = cms.untracked.InputTag("d0selector:D0")
+process.d0ana.MVACollection = cms.InputTag("d0selector:MVAValuesNewD0")
+process.d0ana_wrongsign.useAnyMVA = cms.bool(True)
+process.d0ana_wrongsign.multMin = cms.untracked.double(0)
+process.d0ana_wrongsign.multMax = cms.untracked.double(100000)
+process.d0ana_wrongsign.VertexCompositeCollection = cms.untracked.InputTag("d0selectorWS:D0")
+process.d0ana_wrongsign.MVACollection = cms.InputTag("d0selectorWS:MVAValuesNewD0")
+
+process.d0selectorNewReduced = process.d0selector.clone()
+process.d0selectorNewReduced.GBRForestFileName = cms.string('GBRForestfile_BDT_PromptD0InpPb_default_HLT185_WS_Pt1p5MassPeak_NoPtErrNHitDLAngle2D_v3.root')
+
+process.d0selectorWSNewReduced = process.d0selectorWS.clone()
+process.d0selectorWSNewReduced.GBRForestFileName = cms.string('GBRForestfile_BDT_PromptD0InpPb_default_HLT185_WS_Pt1p5MassPeak_NoPtErrNHitDLAngle2D_v3.root')
+
+process.d0ana_newreduced = process.d0ana.clone()
+process.d0ana_newreduced.VertexCompositeCollection = cms.untracked.InputTag("d0selectorNewReduced:D0")
+process.d0ana_newreduced.MVACollection = cms.InputTag("d0selectorNewReduced:MVAValuesNewD0")
+
+process.d0ana_wrongsign_newreduced = process.d0ana_wrongsign.clone()
+process.d0ana_wrongsign_newreduced.VertexCompositeCollection = cms.untracked.InputTag("d0selectorWSNewReduced:D0")
+process.d0ana_wrongsign_newreduced.MVACollection = cms.InputTag("d0selectorWSNewReduced:MVAValuesNewD0")
+
+
+process.d0ana_seq2 = cms.Sequence(process.eventFilter_HM * process.d0selectorNewReduced * process.d0ana_newreduced)
+process.d0ana_wrongsign_seq2 = cms.Sequence(process.eventFilter_HM * process.d0selectorWSNewReduced * process.d0ana_wrongsign_newreduced)
+
+# eventinfoana must be in EndPath, and process.eventinfoana.selectEvents must be the name of eventFilter_HM Path
+process.eventinfoana.selectEvents = cms.untracked.string('eventFilter_HM_step')
+process.eventinfoana.triggerPathNames = cms.untracked.vstring(
+    'HLT_PAFullTracks_Multiplicity120_v', # High multiplicity
+    'HLT_PAFullTracks_Multiplicity150_v', # High multiplicity
+    'HLT_PAFullTracks_Multiplicity185_part', # High multiplicity
+    'HLT_PAFullTracks_Multiplicity250_v', # High multiplicity
+    'HLT_PAL1MinimumBiasHF_OR_SinglePixelTrack_part', # Minimum bias
+    )
+process.eventinfoana.triggerFilterNames = cms.untracked.vstring()
+process.pevt = cms.EndPath(process.eventinfoana)
+
+process.p = cms.Path(process.d0ana_seq2)
+process.pws = cms.Path(process.d0ana_wrongsign_seq2)
+
+# Add the Conversion tree
+
+# Define the process schedule
+process.schedule = cms.Schedule(
+    process.eventFilter_HM_step,
+    process.d0rereco_step,
+    process.d0rereco_wrongsign_step,
+    process.p,
+    process.pws,
+    process.pevt,
+)
+
+# Add the event selection filters
+process.Flag_colEvtSel = cms.Path(process.eventFilter_HM * process.colEvtSel)
+process.Flag_hfCoincFilter = cms.Path(process.eventFilter_HM * process.hfCoincFilter)
+process.Flag_primaryVertexFilterPA = cms.Path(process.eventFilter_HM * process.primaryVertexFilterPA)
+process.Flag_NoScraping = cms.Path(process.eventFilter_HM * process.NoScraping)
+process.Flag_pileupVertexFilterCut = cms.Path(process.eventFilter_HM * process.olvFilter_pPb8TeV_dz1p0)
+process.Flag_pileupVertexFilterCutGplus = cms.Path(process.eventFilter_HM * process.pileUpFilter_pPb8TeV_Gplus)
+# follow the exactly same config of process.eventinfoana.eventFilterNames
+eventFilterPaths = [ process.Flag_colEvtSel , process.Flag_hfCoincFilter , process.Flag_primaryVertexFilterPA , process.Flag_NoScraping , process.Flag_pileupVertexFilterCut , process.Flag_pileupVertexFilterCutGplus ]
+for P in eventFilterPaths:
+    process.schedule.insert(0, P)
